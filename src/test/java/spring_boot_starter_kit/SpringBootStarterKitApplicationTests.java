c945666d6502d163f8428e34c0c281ea97313005
mason <mason@mason.local>
2026-05-13 18:39:30 +0900

    refact : test activeprofiles 추가

--- src/test/java/spring_boot_starter_kit/SpringBootStarterKitApplicationTests.java
+++ src/test/java/spring_boot_starter_kit/SpringBootStarterKitApplicationTests.java
@@ -2,8 +2,10 @@ package spring_boot_starter_kit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootStarterKitApplicationTests {
 
 	@Test
